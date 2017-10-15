774ec969a1d641508c253bf0774715b003234c21
Your name <Mahima Subramanian>
2017-10-14 22:49:02 -0500

    function added to retrive added info from github API and is rendering

--- main.js
+++ main.js
@@ -6,4 +6,25 @@ function reqListener() {
   let pic = document.querySelector('.pic');
 
   console.log('data', data);
+
+
+let unorderedList = `
+    <li><p>Name:</p><p>${data.name}</p></li>
+    <li><p>GitHub URL:</p><a href="${data.html_url}">${data.login}</a></li>
+    <li><p>Email:</p><p>mahima.subramanian@gmail.com</p></li>
+    <li><p>Company:</p><p>${data.company}</p></li>
+    <li><p>Website:</p><a href="${data.blog}">${data.blog}</a></li>
+    `
+  let story = document.createTextNode(data.bio);
+
+  let profile = `<img src='${data.avatar_url}'>`
+    pic.innerHTML = profile;
+    container.innerHTML = unorderedList;
+    listInfo.appendChild(container);
+    bio.appendChild(story);
 }
+
+let req = new XMLHttpRequest();
+req.open("GET", "https://api.github.com/users/Smahima");
+req.addEventListener("load", reqListener);
+req.send();
